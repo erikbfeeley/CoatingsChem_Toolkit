c748399f9140dfaf115872fca0d21b4620b2731f
Erik Feeley <51027742+erikbfeeley@users.noreply.github.com>
2023-05-02 14:44:09 -0400

    Update hansenCalculations.py

--- hansenCalculations.py
+++ hansenCalculations.py
@@ -1,13 +1,21 @@
-#collection of equations related to Hansen Solubility Parameter equations
+#collection of equations related to Hansen Solubility Parameters
 
 #calculates cohesive energy from dispersion, polar, and hydrogen bonding energies
-def cohesiveEnergyBonds(dispersionEnergy, polarEnergy, hydrogenBondEnergy):
-   cohesiveEnergy = dispersionEnergy + polarEnergy + hydrogenBondEnergy
-   return cohesiveEnergy
+def cohesive_energy_bonds(dispersion_energy, polar_energy, hydrogen_bond_energy):
+   cohesive_energy = dispersion_energy + polar_energy + hydrogen_bond_energy
+   return cohesive_energy
 
 
 #calculates latent heat of vaporization from universal gas constant R and temp in Kelvin K
-def cohesiveEnergyHvap(heatOfVaporization, temp):
-   cohesiveEnergy = heatOfVaporization-(R*temp)
-   cohesiveEnergy
+def cohesive_energy_hvap(heat_of_vaporization, temp):
+   cohesive_energy = heat_of_vaporization-(R*temp)
+   return cohesive_energy
 }
+
+#calculates cohesive energy density from molar volume v_molar and cohesive_energy
+def cohesive_energy_density(v_molar, cohesive_energy):
+   cohesive_energy_density = cohesive_energy / v_molar
+   
+#determines solubility parameter delta from = cohesive energy density
+def solubility_parameter():
+   pass
